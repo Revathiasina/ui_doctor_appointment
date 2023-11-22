cb910e888d80cbe5da530cb5f9b5fc17cee80f69
Revathi <revathiasina13@gmail.com>
2023-11-22 10:29:32 +0000

    doctor appointment fixes

--- src/app/appointment/appointment.component.ts
+++ src/app/appointment/appointment.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AppointmentService } from 'src/api-services/appointment.service';
+import { AppointmentService } from '../api-services/appointment.service';
 
 @Component({
   selector: 'app-appointment',
@@ -9,7 +9,7 @@ import { AppointmentService } from 'src/api-services/appointment.service';
 })
 export class AppointmentComponent {
   myForm: FormGroup
-  slots_list: Record<string, any>[] = [];
+  slots_list: Array<any> = [];
   display_slots: boolean = false;
   specialist = [
     {
@@ -17,12 +17,12 @@ export class AppointmentComponent {
       name: 'General'
     },
     {
-      key: 'heart',
-      name: 'Heart'
+      key: 'heart-surgeons',
+      name: 'Heart Surgeons'
     },
     {
-      key: 'bone',
-      name: 'Bones'
+      key: 'dermatologists',
+      name: 'Dermatologists'
     },
   ]
   constructor(
@@ -51,26 +51,9 @@ export class AppointmentComponent {
       endTime: this.myForm.value.end_time
     }
     if (this.myForm.valid) {
-      // this.slots_list = [
-      //   {
-      //     specialist: 'General',
-      //     doctor: 'Abc',
-      //     slot: '04:00 to 05:00'
-      //   },
-      //   {
-      //     specialist: 'General',
-      //     doctor: 'Xyz',
-      //     slot: '05:00 to 06:00'
-      //   },
-      //   {
-      //     specialist: 'General',
-      //     doctor: 'Def',
-      //     slot: '04:30 to 05:30'
-      //   },
-      // ]
       this.display_slots = true;
       this.myForm.reset()
-      // ***************api to get slots based on the time selected by user *************
+      console.log(body, 'body')
       this._appointment_service.getSlots(body).subscribe({
         next: (res: Record<string, any>) => {
           console.log('res',res);
